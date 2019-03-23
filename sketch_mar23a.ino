226164b651acfa66530af8f6deb070786a4e0074
hfarid2 <43442882+hfarid2@users.noreply.github.com>
2019-03-23 10:54:50 -0500

    our main file

---
+++ sketch_mar23a.ino
@@ -0,0 +1,35 @@
+/*
+ * TechTogetherBoston 
+ * BOSTON LET'S GET I T
+ * Project: 
+ * contributers: Hiba Farid, Tatiana Carbajal, 
+ * WHAT:
+ * WHY:
+ * HOW: 
+ * 
+ * what this code does: 
+ * Applications in the real world:
+ * 
+ */
+
+//step 1: Temperature sensor 
+/* senses temperatures for the phone
+ *  would ideally be added into a phone (or out a little bit like the mic etc.
+ *  this is to just collect data really
+ * 
+ */
+ /*
+  * step 2: if then statements 
+  * fan turns on IF temp is outside of the allowable parameter we set
+  * ????? AT HWAT TEMP DO WE MAKE IT DO THE THING??????
+  */
+
+void setup() {
+  pinMode(8, OUTPUT);
+}
+void loop() {
+  digitalWrite(8, HIGH);   
+  delay(1000);              
+  digitalWrite(8, LOW);   
+  delay(1000);             
+}
