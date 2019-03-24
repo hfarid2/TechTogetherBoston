373ddd640e5c612d4e7a763153a4f5c1e1b21e63
jni77 <46755922+jni77@users.noreply.github.com>
2019-03-24 04:14:58 -0400

    Update and rename sketch_mar23a.ino to demo.ino

--- demo.ino
+++ demo.ino
@@ -2,29 +2,24 @@
  * TechTogetherBoston 
  * BOSTON LET'S GET I T
  * Project: 
- * contributers: Hiba Farid, Tatiana Carbajal, 
- * WHAT:
- * WHY:
- * HOW: 
- * 
- * what this code does: 
- * Applications in the real world:
- * 
+ * contributers: Hiba Farid, Tatiana Carbajal, Jessica Ni 
  */
 
 float temp;
 int tempPin = 0;
+int fanPin = 7;
 int ledPin = 8;
 
 void setup() {
    Serial.begin(9600);
+   pinMode(fanPin, OUTPUT);
    pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
    temp = analogRead(tempPin);
    // read analog volt from sensor and save to variable temp
-   // calibration nonsense ie I Want To Cry
+   // calibration nonsense
    temp -= 200;
    temp /= 25.0;
    // convert the analog volt to its temperature equivalent
@@ -33,12 +28,19 @@ void loop() {
    Serial.print("*C");
    Serial.println();
 
-   // turn on the LED if temperature is above 22 deg 
-   if (temp > 22) {
+   // turn on the fan if temperature is above 22 deg 
+   if (temp < 22) {
      digitalWrite (ledPin, HIGH);
+     digitalWrite (fanPin, LOW);
    }
-   else
+   else if (temp > 22) {
      digitalWrite (ledPin, LOW);
+     digitalWrite (fanPin, HIGH);
+   }
+   else {
+     digitalWrite (ledPin, LOW);
+     digitalWrite (fanPin, LOW);
+   }
    
    delay(3000); // update sensor reading each three seconds
 }
